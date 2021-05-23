84fdaa7a0583c3411b4e219d10b43778693297c7
Ascol9861 <ac.ch503@gmail.com>
2021-05-23 15:01:34 +0545

    created different components

---
+++ src/Components/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import About from './About'
+import Products from './Products'
+
+export default function HomePage() {
+    return (
+     <div>
+         
+     </div>
+    )
+}
