217ec3380372f4c5562cd275420bd7821f967cf6
Ascol9861 <ac.ch503@gmail.com>
2021-05-25 09:21:03 +0545

    created navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Button, Navbar, Nav } from "react-bootstrap";
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 export default function Header() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#deets">More details</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+                Good stuff
+            </Nav.Link>
+        </Nav>
+    </Navbar.Collapse>
+</Navbar>
+  );
 }
